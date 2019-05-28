781b37619df19a2ec97d44dc35cd28c330981ae7
Travis Amaral <travis@konghq.com>
2019-05-28 08:43:51 -0700

    refactor(lambda): re-write promise chain w/ logs

--- functions/getGames.js
+++ functions/getGames.js
@@ -2,13 +2,11 @@ const axios = require('axios')
 const { format } = require('date-fns')
 const nodemailer = require('nodemailer')
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context) => {
   const { MAIL_PASSWORD, MAIL_USER } = process.env
+  console.log(MAIL_USER, MAIL_PASSWORD)
   const url = 'http://gameday.netlify.com/games.json'
   const today = format(new Date(), 'M/DD/YYYY')
-
-  const pass = { statusCode: 200, body: 'Email sent!' }
-  const fail = { statusCode: 404, body: 'No game' }
 
   // Setup SMTP details
   const transporter = nodemailer.createTransport({
@@ -18,15 +16,10 @@ exports.handler = async (event, context, callback) => {
     auth: { MAIL_USER, MAIL_PASSWORD }
   })
 
-  return axios.get(url)
-    .then(res => {
-      const game = res.data[0][today] // Get today's game if exists
-
-      if (!game) {
-        console.log('no game')
-        return callback(null, fail)
-      } else {
-        console.log('game')
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(res => {
+        const game = res.data[0]['4/28/2019'] // Get today's game if exists
         const mailOptions = {
           from: `"Travis Amaral" <travispamaral@gmail.com>`,
           to: 'travispamaral@gmail.com',
@@ -36,20 +29,38 @@ exports.handler = async (event, context, callback) => {
           <p><a href="https://gameday.netlify.com">https://gameday.netlify.com</p>
           `
         }
-        // Send the email
+
+        console.log(game)
+
+        if (!game) {
+          console.log('no game')
+          resolve({
+            statusCode: 200,
+            body: 'No Game Today'
+          })
+        }
+
         transporter.sendMail(mailOptions, function (error, info) {
           if (error) {
-            return callback(null, {
+            console.log('Error sending email')
+            resolve({
               statusCode: 500,
-              body: JSON.stringify({
-                error: error.message
-              })
+              body: JSON.stringify(error)
             })
           }
 
-          return callback(null, pass)
+          resolve({
+            statusCode: 200,
+            body: 'Email sent!'
+          })
         })
-      }
-    })
-    .catch(error => callback(null, { statusCode: 400, body: JSON.stringify({ error: error.message }) }))
+      })
+      .catch(error => {
+        console.log(error)
+        resolve({
+          statusCode: 400,
+          body: JSON.stringify({ error: error.message })
+        })
+      })
+  })
 }
