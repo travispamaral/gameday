f8c6c5f1e9a38f8377746b4a4636210bb3b2aa3d
Travis Amaral <travis@konghq.com>
2019-05-28 09:04:52 -0700

    feat(lambda): swap to OAuth instead of plain

--- functions/getGames.js
+++ functions/getGames.js
@@ -1,19 +1,38 @@
 const axios = require('axios')
 const { format } = require('date-fns')
+const { google } = require('googleapis')
 const nodemailer = require('nodemailer')
+const OAuth2 = google.auth.OAuth2
 
-exports.handler = (event, context) => {
-  const { MAIL_PASSWORD, MAIL_USER } = process.env
-  console.log(MAIL_USER, MAIL_PASSWORD)
+exports.handler = async (event, context) => {
+  const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
   const url = 'http://gameday.netlify.com/games.json'
   const today = format(new Date(), 'M/DD/YYYY')
+  const refreshToken = '1/mii74FRfifcgKeGVbZUYKBzTZw9ZnvJfk7l-aIhyEKY'
+
+  const oauth2Client = new OAuth2(
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    'https://developers.google.com/oauthplayground'
+  )
+
+  oauth2Client.setCredentials({
+    refresh_token: refreshToken
+  })
+  const tokens = await oauth2Client.refreshAccessToken()
+  const accessToken = tokens.credentials.access_token
 
   // Setup SMTP details
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: { MAIL_USER, MAIL_PASSWORD }
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'travispamaral@gmail.com',
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      refreshToken,
+      accessToken
+    }
   })
 
   return new Promise((resolve, reject) => {
@@ -29,8 +48,6 @@ exports.handler = (event, context) => {
           <p><a href="https://gameday.netlify.com">https://gameday.netlify.com</p>
           `
         }
-
-        console.log(game)
 
         if (!game) {
           console.log('no game')
@@ -48,7 +65,6 @@ exports.handler = (event, context) => {
               body: JSON.stringify(error)
             })
           }
-
           resolve({
             statusCode: 200,
             body: 'Email sent!'
